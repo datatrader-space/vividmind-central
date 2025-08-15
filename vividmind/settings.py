936fc04f45f1df30dcbf8de8ac553044e33b249d
hamza <hamza@datatrader.space>
2025-08-15 12:52:19 +0500

    update settings

--- vividmind/settings.py
+++ vividmind/settings.py
@@ -175,7 +175,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-REDIS_HOST = config('REDIS_HOST')
+
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
